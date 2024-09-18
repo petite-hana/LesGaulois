a6f97060353be8b2f2df5535f132b5d8067a1902
Hana Descazeaux <hana.descazeaux@univ-tlse3.fr>
2024-09-19 00:41:14 +0200

    Creation des classes personnages

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //TODO créer un main permettant de tester la classe Gaulois
         System.out.println("Hello world!");
     }
 }
