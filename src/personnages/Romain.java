91ac0ab7d1e82680bd035c5e5ec92a1e2512475e
Hana Descazeaux <hana.descazeaux@univ-tlse3.fr>
2024-09-23 18:47:24 +0200

    TP2: ajout des equipements pour les Romain

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,10 +3,15 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipement = 0;
 
 	public Romain(String nom, int force) {
+		assert force > 0;
+
 		this.nom = nom;
 		this.force = force;
+		this.equipements = new Equipement[Equipement.values().length];
 	}
 
 	public String getNom() {
@@ -34,9 +39,30 @@ public class Romain {
 		}
 	}
 
+	public void sEquiper(Equipement equipement) {
+		if (nbEquipement == 2) {
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+			return;
+		}
+
+		if (equipements[equipement.ordinal()] != null) {
+			System.out.println("Le soldat " + nom + " possède déjà un " + equipement.nom + " !");
+			return;
+		}
+
+		System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement.nom + " !");
+		equipements[equipement.ordinal()] = equipement;
+		nbEquipement++;
+	}
+
 	// FONCTION MAIN
 	public static void main(String[] args) {
-		Romain romain = new Romain("Rominus", 3);
+		Romain romain = new Romain("Minus", 3);
+
+		romain.sEquiper(Equipement.CASQUE);
+		romain.sEquiper(Equipement.CASQUE);
+		romain.sEquiper(Equipement.BOUCLIER);
+		romain.sEquiper(Equipement.BOUCLIER);
 
 		romain.parler("Je suis un romain");
 		romain.recevoirCoup(1);
